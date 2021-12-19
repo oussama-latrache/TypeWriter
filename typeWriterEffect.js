9de937bb2ba1eb1edda55447aca578485a2c6414
oussama-latrache <82266847+oussama-latrache@users.noreply.github.com>
2021-12-19 13:59:52 +0100

    we just dit the text and the colors

---
+++ typeWriterEffect.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["Oussama Latrache", "Front end Developer", "Python Developer"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+  };
+  
+  var typed = new Typed(".title", options);
